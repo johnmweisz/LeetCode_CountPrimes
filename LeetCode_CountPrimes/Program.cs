2eed8224b3268952c9fc4dabc5aa4cd7bcd3e384
John Weisz <johnmweisz@gmail.com>
2019-12-12 17:14:42 -0800

    Add project files.

---
+++ LeetCode_CountPrimes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CountPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.CountPrimes(10));
+        }
+    }
+    public class Solution
+    {
+        public static int CountPrimes(int n)
+        {
+            var notPrimes = new bool[n + 1];
+            var count = 0;
+            notPrimes[0] = true;
+            notPrimes[1] = true;
+            for (var i = 2; i < notPrimes.Length; i++)
+            {
+                if (notPrimes[i]) continue;
+                count++;
+                for (var j = 2; j * i <= n; j++)
+                {
+                    notPrimes[i * j] = true;
+                }
+            }
+            return count;
+        }
+    }
+}
